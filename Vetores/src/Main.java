0f5b5b19b06cd4bfb1fb23dcd6c8b6920a64c9c1
Sam06tanb <78392140+sam06tanb@users.noreply.github.com>
2023-10-29 18:10:59 -0300

    Add files via upload

---
+++ Vetores/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+            Scanner sc = new Scanner(System.in);
+
+
+
+            int n = sc.nextInt();
+            double[] vect = new double[n];
+
+            for (int i=0; i<n; i++) {
+                vect[i] = sc.nextDouble();
+            }
+
+            double sum = 0.0;
+            for (int i=0; i<n; i++) {
+                sum += vect[i];
+            }
+            double avg = sum / n;
+
+            System.out.printf("Average Height: %.2f%n", + avg);
+
+
+
+
+
+
+        sc.close();
+        }
+    }
